63f16606a5f6ffe363e1d694aea337743d9d05fb
Thomas Boittin <tboittin.pro@gmail.com>
2021-01-23 15:50:23 +0100

    final state

---
+++ script.js
@@ -0,0 +1,19 @@
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+let load = 0
+
+const blurring = () => {
+  load++
+
+  if (load > 99) {
+    clearInterval(int)
+  }
+
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = 1 - load / 100
+
+  bg.style.filter = `blur(${30 * (1 - load / 100)}px)`
+}
+
+let int = setInterval(blurring, 30)
